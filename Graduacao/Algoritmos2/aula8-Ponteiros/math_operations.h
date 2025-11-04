76407bfdee02795a4dff7ec2969e3d9abfd3fed5
André Luiz Clemente de Oliveira <andreluizcle@gmail.com>
2025-11-04 18:41:35 -0300

    Create math_operations.h

---
+++ Graduacao/Algoritmos2/aula8-Ponteiros/math_operations.h
@@ -0,0 +1,7 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+    int add(int a, int b){return a+b;}
+    int sub(int a, int b){return a-b;}
+    int multi(int a, int b){return a*b;}
+    int divi(int a, int b){return a/b;}
+#endif
